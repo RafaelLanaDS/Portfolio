acb665e33daad50f425ddbfb5bd21ae49cc85f43
RafaelLanaDS <134884057+RafaelLanaDS@users.noreply.github.com>
2026-05-26 22:54:26 -0300

    commit

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,11 @@
 $(document).on('click', '.nav-scroll', function(e) {
-    e.preventDefault(); // Impede o salto brusco padrão
+    e.preventDefault();
+    const target = $(this).attr('href');
 
-    const target = $(this).attr('href'); // Pega o "#home", "#projetos", etc.
+    // Home vai pro topo absoluto, demais seções descontam a navbar
+    const offset = target === '#home' ? 0 : 65;
 
     $('html, body').animate({
-        scrollTop: $(target).offset().top - 70 // -70 = altura do navbar fixo
-    }, 900); // 900ms de duração da animação
+        scrollTop: $(target).offset().top - offset
+    }, 900);
 });
